a615e1c4508e091c810cd65180f040d7798cb3c3
JacquelineMai <35640340+JacquelineMai@users.noreply.github.com>
2020-01-17 22:12:40 -0800

    jm/bt - made starter project w/ spring initializer

---
+++ labscheduler/src/main/java/edu/ucsb/cs56/labscheduler/LabschedulerApplication.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs56.labscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabschedulerApplication.class, args);
+	}
+
+}
---
+++ labscheduler/src/test/java/edu/ucsb/cs56/labscheduler/LabschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs56.labscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
