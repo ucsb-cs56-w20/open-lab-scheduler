5544f9ea6cf34d35f5648ccde67446422ffc9dcc
sarawang <369278452@qq.com>
2020-02-13 13:25:41 -0800

    pw&jm - add course offering controller

---
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/CourseOfferingController.java
@@ -0,0 +1,63 @@
+package edu.ucsb.cs56.ucsb_open_lab_scheduler.controllers;
+
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.advice.AuthControllerAdvice;
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.entities.CourseOffering;
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.repositories.CourseOfferingRepository;
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.services.CSVToCourseOfferingServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+
+@Controller
+public class CourseOfferingController {
+    private static Logger log = LoggerFactory.getLogger(CourseOfferingController.class);
+
+    @Autowired
+    private AuthControllerAdvice authControllerAdvice;
+
+    @Autowired
+    CSVToCourseOfferingServiceImpl csvtco;
+
+    @Autowired
+    CourseOfferingRepository courseOfferingRepository;
+
+    @GetMapping("/courseOffering")
+    public String dashboard(Model model, OAuth2AuthenticationToken token, RedirectAttributes redirAttrs) {
+        String role = authControllerAdvice.getRole(token);
+        if (!role.equals("Admin")) {
+            redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
+            return "redirect:/";
+        }
+        model.addAttribute("CourseOfferingModel", courseOfferingRepository.findAll());
+        return "courseOffering";
+    }
+
+    @PostMapping("/courseOffering/upload")
+    public String uploadCSV(@RequestParam("csv") MultipartFile csv, OAuth2AuthenticationToken token, RedirectAttributes redirAttrs) {
+        String role = authControllerAdvice.getRole(token);
+        if (!role.equals("Admin")) {
+            redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
+            return "redirect:/";
+        }
+        try(Reader reader = new InputStreamReader(csv.getInputStream())){
+            List<CourseOffering> courseOfferings = csvtco.parse(reader);
+            courseOfferingRepository.saveAll(courseOfferings);
+        }catch(IOException e){
+            log.error(e.toString());
+        }
+        return "redirect:/courseOffering";
+    }
+}
