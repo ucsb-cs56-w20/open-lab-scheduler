0e082023ecb492bd20827485b61d32ae5b7c9ae0
Scott Chow <tehchowster@gmail.com>
2020-02-14 14:13:01 -0800

    sc/team - add course offering indirection page for tutor assignments

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/TutorAssignmentController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/TutorAssignmentController.java
@@ -18,26 +18,24 @@ import edu.ucsb.cs56.ucsb_open_lab_scheduler.repositories.CourseOfferingReposito
 import edu.ucsb.cs56.ucsb_open_lab_scheduler.advice.AuthControllerAdvice;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 
-
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Iterator;
+import java.util.Optional;
 
 @Controller
 public class TutorAssignmentController {
 
   private Logger logger = LoggerFactory.getLogger(TutorAssignmentController.class);
-
   private final TutorRepository tutorRepository;
   private final CourseOfferingRepository courseOfferingRepository;
+  private final TutorAssignmentRepository tutorAssignmentRepository;
 
   @Autowired
   private AuthControllerAdvice authControllerAdvice;
-  @Autowired
-  private final TutorAssignmentRepository tutorAssignmentRepository;
 
   public TutorAssignmentController(TutorAssignmentRepository tutorAssignmentRepository, TutorRepository tutorRepository,
       CourseOfferingRepository courseOfferingRepository) {
@@ -53,29 +51,53 @@ public class TutorAssignmentController {
       redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
       return "redirect:/";
     }
-    model.addAttribute("TutorAssignmentModel", tutorAssignmentRepository.findAll());
+    model.addAttribute("courseOfferings", courseOfferingRepository.findAll());
     return "tutorAssignment";
   }
 
-  @PostMapping("/tutorAssignment/add")
-  public String add(@RequestParam(name = "cid") long cid, @RequestParam(name = "tid") long tid, Model model) {
-    Tutor tutor = tutorRepository.findById(tid)
-        .orElseThrow(() -> new IllegalArgumentException("Invalid tutor Id:" + tid));
-    CourseOffering courseOffering = courseOfferingRepository.findById(cid)
-        .orElseThrow(() -> new IllegalArgumentException("Invalid course offering Id:" + cid));
+  @GetMapping("/tutorAssignment/{id}")
+  public String manageCourse(@PathVariable("id") long id, Model model, OAuth2AuthenticationToken token,
+      RedirectAttributes redirAttrs) {
+    String role = authControllerAdvice.getRole(token);
+    if (!role.equals("Admin")) {
+      redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
+      return "redirect:/";
+    }
 
-    TutorAssignment tutorAssignment = new TutorAssignment(tutor, courseOffering);
-    tutorAssignmentRepository.save(tutorAssignment);
-    model.addAttribute("tutorAssignment", tutorAssignmentRepository.findAll());
-    return "tutorAssignment";
+    Optional<CourseOffering> courseOffering = courseOfferingRepository.findById(id);
+    if (!courseOffering.isPresent()) {
+      redirAttrs.addFlashAttribute("alertDanger", "Course offering with id " + id + " not found");
+      return "redirect:/";
+
+    }
+
+    model.addAttribute("courseOffering", courseOffering.get());
+
+    return "tutorAssignment/manage";
   }
 
-  @GetMapping("/tutorAssignment/delete/{id}")
-  public String delete(@PathVariable("id") long id, Model model) {
-    TutorAssignment tutorAssignment = tutorAssignmentRepository.findById(id)
-        .orElseThrow(() -> new IllegalArgumentException("Invalid tutor assignment Id:" + id));
-    tutorAssignmentRepository.delete(tutorAssignment);
-    return "tutorAssignment";
-  }
+  // @PostMapping("/tutorAssignment/add")
+  // public String add(@RequestParam(name = "cid") long cid, @RequestParam(name =
+  // "tid") long tid, Model model) {
+  // Tutor tutor = tutorRepository.findById(tid)
+  // .orElseThrow(() -> new IllegalArgumentException("Invalid tutor Id:" + tid));
+  // CourseOffering courseOffering = courseOfferingRepository.findById(cid)
+  // .orElseThrow(() -> new IllegalArgumentException("Invalid course offering Id:"
+  // + cid));
+
+  // TutorAssignment tutorAssignment = new TutorAssignment(tutor, courseOffering);
+  // tutorAssignmentRepository.save(tutorAssignment);
+  // model.addAttribute("tutorAssignment", tutorAssignmentRepository.findAll());
+  // return "tutorAssignment";
+  // }
+
+  // @GetMapping("/tutorAssignment/delete/{id}")
+  // public String delete(@PathVariable("id") long id, Model model) {
+  // TutorAssignment tutorAssignment = tutorAssignmentRepository.findById(id)
+  // .orElseThrow(() -> new IllegalArgumentException("Invalid tutor assignment
+  // Id:" + id));
+  // tutorAssignmentRepository.delete(tutorAssignment);
+  // return "tutorAssignment";
+  // }
 
 }
