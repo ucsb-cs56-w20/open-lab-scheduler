605f1535fde4b4009efb665b6f87c53efee6f439
sarawang <369278452@qq.com>
2020-02-13 22:29:02 -0800

    Revert "pw - use one single class for CSV to Object Service"
    This reverts commit b836af21d502f66b9b900565e4427b0192e5fd64.

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/services/CSVToObjectService.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/services/CSVToObjectService.java
@@ -8,16 +8,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.List;
+import java.util.Objects;
 
 
-public interface CSVToObjectService<T> {
-    List<T> parse(Reader csv);
+public interface CSVToObjectService {
+    <T extends Object> List<T> parse(Reader csv);
 
     Logger getLogger();
 
-    void setType(Class<T> type);
-
-    default List<T> parse(MultipartFile file) {
+    default <T extends Object> List<T> parse(MultipartFile file) {
         try {
             return parse(new InputStreamReader(file.getInputStream()));
         } catch (IOException e) {
