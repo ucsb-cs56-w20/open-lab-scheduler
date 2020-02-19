f9b651821252d7498267d5d410df7c1ae9c92048
sarawang <369278452@qq.com>
2020-02-18 21:22:02 -0800

    pw - fix the close stream issue

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/services/CSVToObjectService.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/services/CSVToObjectService.java
@@ -16,13 +16,23 @@ public interface CSVToObjectService<T> {
     Logger getLogger();
 
     default List<T> parse(MultipartFile file, Class<T> type) {
+        InputStreamReader reader = null;
         try {
-            InputStreamReader reader = new InputStreamReader(file.getInputStream());
+            reader = new InputStreamReader(file.getInputStream());
             List<T> result = parse(reader, type);
             reader.close();
             return result;
         } catch (IOException e) {
             getLogger().error("CSV could not be parsed", e);
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+                    // ignore for now as any significant errors 
+                    // should already have been reported
+                }
+            }
         }
         return null;
     }
