ec2e07b34a9f6d80629f1edea3d2d82481350493
Yuval Steinhart <yuval@cs.ucsb.edu>
2020-02-26 14:28:35 -0800

    ys/lr-implemented custom SQL query in RoomAvailabilityRepository

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/RoomAvailabilityRepository.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/RoomAvailabilityRepository.java
@@ -1,10 +1,10 @@
 package edu.ucsb.cs56.ucsb_open_lab_scheduler.repositories;
 
 import edu.ucsb.cs56.ucsb_open_lab_scheduler.entities.RoomAvailability;
+import java.util.List;
 import edu.ucsb.cs56.ucsb_open_lab_scheduler.entities.TimeSlotAssignment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
