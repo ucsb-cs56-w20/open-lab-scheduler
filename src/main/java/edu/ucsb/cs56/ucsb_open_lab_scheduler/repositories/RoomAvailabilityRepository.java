9cf26b67a6eeab79bf52bbf84e25e38009c0a933
Yuval Steinhart <yuval@cs.ucsb.edu>
2020-02-26 14:28:35 -0800

    ys/lr-implemented custom SQL query in RoomAvailabilityRepository

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/RoomAvailabilityRepository.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/RoomAvailabilityRepository.java
@@ -1,14 +1,17 @@
 package edu.ucsb.cs56.ucsb_open_lab_scheduler.repositories;
 
 import edu.ucsb.cs56.ucsb_open_lab_scheduler.entities.RoomAvailability;
-
-import java.util.List;
-
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.entities.TimeSlotAssignment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface RoomAvailabilityRepository extends CrudRepository<RoomAvailability, Long> {
     RoomAvailability findById(long id);
-    List<RoomAvailability> findByQuarter(String quarter);
+
+    @Query(value = "SELECT * from TimeSlotAssignment WHERE tutorId IN (SELECT tutorId from TutorAssignment WHERE courseOfferingId = id)", nativeQuery=true)
+	List<TimeSlotAssignment> getTimeSlotAssignments(@Param("id") String id);
 }
