5d40d2343122c7d25cee25edb7eff61023471212
Aaron Huang <aaronhuang@umail.ucsb.edu>
2020-03-02 15:02:55 -0800

    AH - further fixes to nav bar

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/TutorViewController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/TutorViewController.java
@@ -1,27 +1,30 @@
 package edu.ucsb.cs56.ucsb_open_lab_scheduler.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.List;
-
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.entities.Tutor;
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.entities.TutorAssignment;
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.entities.CourseOffering;
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.repositories.TutorAssignmentRepository;
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.repositories.TutorRepository;
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.repositories.CourseOfferingRepository;
 
 import edu.ucsb.cs56.ucsb_open_lab_scheduler.advice.AuthControllerAdvice;
-import edu.ucsb.cs56.ucsb_open_lab_scheduler.entities.Tutor;
-import edu.ucsb.cs56.ucsb_open_lab_scheduler.repositories.TutorRepository;
-import edu.ucsb.cs56.ucsb_open_lab_scheduler.services.CSVToObjectService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 @Controller
 public class TutorViewController {
