11f0cbf7a07817d4d6efed4336fec5e3f946d614
Yuval Steinhart <yuval@cs.ucsb.edu>
2020-03-03 12:44:30 -0800

    ys - added various queries in the RoomAvailibilityRepository and TutorAssignmentRepository, created TimeSlotAssignmentRepository

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/TimeSlotAssignmentRepository.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/TimeSlotAssignmentRepository.java
@@ -10,7 +10,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface TimeSlotAssignmentRepository extends CrudRepository<TimeSlotAssignment, Long> {
