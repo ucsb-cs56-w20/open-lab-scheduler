8191550deae3ba9ce4a63ec63daf0a890bc32c71
Christopher Scott <clscott@umail.ucsb.edu>
2020-03-03 10:43:42 -0800

    cs edits for delete function

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/CourseOfferingController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/CourseOfferingController.java
@@ -11,6 +11,7 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -46,7 +47,8 @@ public class CourseOfferingController {
     }
 
     @PostMapping("/courseOffering/upload")
-    public String uploadCSV(@RequestParam("csv") MultipartFile csv, OAuth2AuthenticationToken token, RedirectAttributes redirAttrs) {
+    public String uploadCSV(@RequestParam("csv") MultipartFile csv, OAuth2AuthenticationToken token,
+            RedirectAttributes redirAttrs) {
         String role = authControllerAdvice.getRole(token);
         if (!(role.equals("Admin"))) {
             redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
@@ -55,9 +57,21 @@ public class CourseOfferingController {
         try(Reader reader = new InputStreamReader(csv.getInputStream())){
             List<CourseOffering> courseOfferings =  csvToObjectService.parse(reader, CourseOffering.class);
             courseOfferingRepository.saveAll(courseOfferings);
-        }catch(IOException e){
+        } catch (IOException e) {
             log.error(e.toString());
         }
         return "redirect:/courseOffering";
+    }
+
+    @GetMapping("/courseOffering/delete/{id}")
+    public String deleteCourse(@PathVariable("id") long id, Model model, OAuth2AuthenticationToken token,
+            RedirectAttributes redirAttrs) {
+        String role = authControllerAdvice.getRole(token);
+        if (!role.equals("Admin")) {
+            redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
+            return "redirect:/";
+        }
+        model.addAttribute("CourseOfferingModel", courseOfferingRepository.findById(id));
+        return "courseOffering/delete";
     }
 }
