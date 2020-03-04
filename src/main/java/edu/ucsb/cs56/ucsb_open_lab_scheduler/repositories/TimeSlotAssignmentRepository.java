f32b9e8fcbfa5a53a3e0acfe8caf316348ede680
Yuval Steinhart <yuval@cs.ucsb.edu>
2020-03-04 15:47:41 -0800

    ys - modified some syntax errors

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/TimeSlotAssignmentRepository.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/TimeSlotAssignmentRepository.java
@@ -9,10 +9,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface TimeSlotAssignmentRepository extends CrudRepository<TimeSlotAssignment, Long> {
-    @Query(value = "SELECT * FROM TimeSlotAssignment WHERE tutor IN (SELECT tutor from (SELECT * from TutorAssignment WHERE CourseOffering IN (SELECT CourseOffering from TutorAssignment WHERE courseOfferingId = courseId AND quarter = courseQuarter)))", nativeQuery=true)
-	List<TimeSlotAssignment> getTimeSlotAssignmentsByCourseIdAndQuarter(@Param("courseId") String courseId, @Param("courseQuarter") String quarter);
+    
+    List<TimeSlotAssignment> findByTutorAndCourseOffering(Tutor tutor, CourseOffering courseOffering);
+
 }
