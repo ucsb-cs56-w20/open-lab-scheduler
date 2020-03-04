a980c6c9ecc3361b12971eca44fcb0aff70629cc
Christopher Scott <clscott@umail.ucsb.edu>
2020-03-04 12:24:14 -0800

    cs - removed duplicate methods

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/CourseOfferingController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/CourseOfferingController.java
@@ -112,47 +112,4 @@ public class CourseOfferingController {
         return "redirect:/courseOffering/";
 
     }
-
-    @GetMapping("/courseOffering/delete/{id}")
-    public String deleteConfirm(@PathVariable("id") long id, Model model, OAuth2AuthenticationToken token,
-            RedirectAttributes redirAttrs) {
-        String role = authControllerAdvice.getRole(token);
-        if (!role.equals("Admin")) {
-            redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
-            return "redirect:/";
-        }
-
-        Optional<CourseOffering> course = courseOfferingRepository.findById(id);
-        if (!course.isPresent()) {
-            redirAttrs.addFlashAttribute("alertDanger", "Course with that id does not exist.");
-        } else {
-            model.addAttribute("CourseToDelete", courseOfferingRepository.findById(id).get());
-        }
-
-        return "courseOffering/confirmDelete";
-    }
-
-    @PostMapping("/courseOffering/delete/confirm/{id}")
-    public String deleteCourse(@PathVariable("id") long id, Model model, OAuth2AuthenticationToken token,
-            RedirectAttributes redirAttrs) {
-        String role = authControllerAdvice.getRole(token);
-        if (!role.equals("Admin")) {
-            redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
-            return "redirect:/";
-        }
-        Optional<CourseOffering> course = courseOfferingRepository.findById(id);
-        if (!course.isPresent()) {
-            redirAttrs.addFlashAttribute("alertDanger", "Course with that id does not exist.");
-        } else {
-
-            List<TutorAssignment> tutorAssignments = tutorAssignmentRepository.findByCourseOffering(course.get());
-            for (TutorAssignment tutorAssignment : tutorAssignments) {
-                tutorAssignmentRepository.delete(tutorAssignment);
-            }
-            courseOfferingRepository.delete(course.get());
-            redirAttrs.addFlashAttribute("alertSuccess", "Course successfully deleted.");
-        }
-        return "redirect:/courseOffering/";
-
-    }
 }
