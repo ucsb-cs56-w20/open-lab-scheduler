f88ed7eba20382919f6284044dbe2246091efd82
robg1638 <rgutierrez555@umail.ucsb.edu>
2020-03-03 16:26:03 -0800

    rg - added dropdwon menus for Quarters and Courses to homepage, need to fix Quarter menu

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/CourseOfferingRepository.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/CourseOfferingRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CourseOfferingRepository extends CrudRepository<CourseOffering, Long> {
     //List<CourseOffering> getCourseOfferings();
+    public List<CourseOffering> findByInstructorEmail(String instructorEmail);
 }
