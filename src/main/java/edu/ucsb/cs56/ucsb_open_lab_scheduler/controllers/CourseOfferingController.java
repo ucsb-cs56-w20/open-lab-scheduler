e9baf0ceb122af0415f6adc29afbd27d9bfbf4e9
Christopher Scott <clscott@umail.ucsb.edu>
2020-03-03 19:06:01 -0800

    cs added delete function

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/CourseOfferingController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/CourseOfferingController.java
@@ -102,16 +102,4 @@ public class CourseOfferingController {
         return "redirect:/courseOffering/courseOffering";
 
     }
-
-    @GetMapping("/courseOffering/delete/{id}")
-    public String deleteCourse(@PathVariable("id") long id, Model model, OAuth2AuthenticationToken token,
-            RedirectAttributes redirAttrs) {
-        String role = authControllerAdvice.getRole(token);
-        if (!role.equals("Admin")) {
-            redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
-            return "redirect:/";
-        }
-        model.addAttribute("CourseOfferingModel", courseOfferingRepository.findById(id));
-        return "courseOffering/delete";
-    }
 }
