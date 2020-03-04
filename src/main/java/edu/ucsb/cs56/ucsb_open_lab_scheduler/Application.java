a39019cbe0e7712e13b686202ad62267b892369b
zacharyfriedland <zacharyfriedland@ucsb.edu>
2020-03-03 18:34:41 -0800

    zf - removed extras in Application.java

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/Application.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/Application.java
@@ -1,64 +1,11 @@
 package edu.ucsb.cs56.ucsb_open_lab_scheduler;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
-import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 
 
-/*
-class CustomAuthorizationRequestResolver implements OAuth2AuthorizationRequestResolver {
-
-    private final OAuth2AuthorizationRequestResolver defaultResolver;
-
-    public CustomAuthorizationRequestResolver(ClientRegistrationRepository repo,
-            String authorizationRequestBaseUri) {
-        defaultResolver = new DefaultOAuth2AuthorizationRequestResolver(repo, authorizationRequestBaseUri);
-    }
-    // ...
-
-    @Override
-    public OAuth2AuthorizationRequest resolve(final HttpServletRequest request) {
-        OAuth2AuthorizationRequest req = defaultResolver.resolve(request);
-        if (req != null) {
-            req = customizeAuthorizationRequest(req);
-        }
-        return req;
-    }
-
-    @Override
-    public OAuth2AuthorizationRequest resolve(final HttpServletRequest request, final String clientRegistrationId) {
-        OAuth2AuthorizationRequest req = defaultResolver.resolve(request, clientRegistrationId);
-        if (req != null) {
-            req = customizeAuthorizationRequest(req);
-        }
-        return req;
-    }
-
-    private OAuth2AuthorizationRequest customizeAuthorizationRequest(final OAuth2AuthorizationRequest req) {
-        // ...
-        final Map<String, Object> extraParams = new HashMap<String, Object>();
-        extraParams.putAll(req.getAdditionalParameters());
-        extraParams.put("hd", "uscb.edu");
-
-        return OAuth2AuthorizationRequest.from(req).additionalParameters(extraParams).build();
-    }
-}*/
 
 @SpringBootApplication
 
@@ -69,9 +16,6 @@ public class Application extends WebSecurityConfigurerAdapter {
     }
 
     private static final String HOSTED_DOMAIN_ATTRIBUTE = "hd";
-
-    @Value("#{'${allowed-domains}'.split(',')}")
-    private List<String> allowedDomains;
     
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
