50b6b85464b97c7694c1f3a02d9041e4e5b40fae
Tanay Komarlu <tkomarlu@ucsb.edu>
2020-03-03 21:34:51 -0800

    tk - removed wildcard

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/RoomAvailabilityController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/RoomAvailabilityController.java
@@ -12,13 +12,15 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 
 
 import java.io.IOException;
