9e20607f6b248429a1a1e049883948a80c413224
Patrick Kuang <pkuang@ucsb.edu>
2020-03-05 15:42:05 -0800

    pk - added csv download controller and userstocsv entity

---
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/CSVDownloadController.java
@@ -0,0 +1,33 @@
+package edu.ucsb.cs56.ucsb_open_lab_scheduler.controllers;
+
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.entities.User;
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.entities.UsersToCSV;
+import edu.ucsb.cs56.ucsb_open_lab_scheduler.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CSVDownloadController {
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/tutorCSV")
+    public void downloadCSV(HttpServletResponse response) throws IOException{
+        response.setContentType("text/csv");
+        response.setHeader("Content-Disposition", "attachment; file=tutors.csv");
+
+        List<User> tutors =  (List<User>) userRepository.findAll();
+
+        UsersToCSV.writeTutors(response.getWriter(), tutors);
+    }
+}
---
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/entities/UsersToCSV.java
@@ -0,0 +1,40 @@
+package edu.ucsb.cs56.ucsb_open_lab_scheduler.entities;
+import java.io.PrintWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.opencsv.CSVWriter;
+
+import java.util.List;
+
+public class UsersToCSV {
+    private static Logger logger = LoggerFactory.getLogger(UsersToCSV.class);
+    public static void writeTutors(PrintWriter writer, List<User> users) {
+        String[] CSV_HEADER = {"id", "firstname", "lastname", "email"};
+        try (
+                CSVWriter csvWriter = new CSVWriter(writer,
+                        CSVWriter.DEFAULT_SEPARATOR,
+                        CSVWriter.NO_QUOTE_CHARACTER,
+                        CSVWriter.DEFAULT_ESCAPE_CHARACTER,
+                        CSVWriter.DEFAULT_LINE_END);
+        ) {
+            csvWriter.writeNext(CSV_HEADER);
+
+            for (User user : users) {
+                String[] data = {
+                        String.valueOf(user.getId()),
+                        user.getFirstName(),
+                        user.getLastName(),
+                        user.getEmail()
+                };
+
+                csvWriter.writeNext(data);
+            }
+            csvWriter.close();
+            logger.info("Write CSV using CSVWriter successfully!");
+        } catch (Exception e) {
+            logger.info("Writing CSV error!");
+            e.printStackTrace();
+        }
+    }
+}
