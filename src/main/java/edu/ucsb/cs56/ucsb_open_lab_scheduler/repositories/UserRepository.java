a4c92e9c595412943fd7fb8aaa946940969b1083
qiandrew <andrew99qi@gmail.com>
2020-03-05 19:05:30 -0800

    ahhhhhh

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/UserRepository.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/repositories/UserRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-  Optional<User> findByEmail(String email);
+  Optional<User> findByEmail(String Email);
 }
