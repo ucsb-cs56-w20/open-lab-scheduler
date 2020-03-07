97d837244ac851f6dba920db597dce0b51132d6f
zacharyfriedland <zacharyfriedland@ucsb.edu>
2020-03-07 15:27:50 -0800

    Added case where there might be a combination of Admin/Tutor/Instructor

--- src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/TutorSignUpController.java
+++ src/main/java/edu/ucsb/cs56/ucsb_open_lab_scheduler/controllers/TutorSignUpController.java
@@ -64,7 +64,7 @@ public class TutorSignUpController{
     @GetMapping("/tutorSignUp/courseSelect")
     public String signUpTable(Model model, OAuth2AuthenticationToken token, RedirectAttributes redirAttrs) {
       String role = authControllerAdvice.getRole(token);
-      if (!role.equals("Tutor")) {
+      if (!authControllerAdvice.getIsTutor(token)) {
         redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
         return "redirect:/";
       }
@@ -91,7 +91,7 @@ public class TutorSignUpController{
     @GetMapping("/tutorSignUp/courseSelect/{id}")
     public String timeSlotAssignMent(@PathVariable("id") long id, Model model, OAuth2AuthenticationToken token, RedirectAttributes redirAttrs){
         String role = authControllerAdvice.getRole(token);
-        if (!role.equals("Tutor")) {
+        if (!authControllerAdvice.getIsTutor(token)) {
         redirAttrs.addFlashAttribute("alertDanger", "You do not have permission to access that page");
         return "redirect:/";
         }
@@ -135,7 +135,7 @@ public class TutorSignUpController{
     public ResponseEntity<?> add(@RequestParam("sid") long sid, @RequestParam("tid") long tid,
                                 OAuth2AuthenticationToken token) {
         String role = authControllerAdvice.getRole(token);
-        if (!role.equals("Tutor")) {
+        if (!authControllerAdvice.getIsTutor(token)) {
             return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
 
@@ -153,7 +153,7 @@ public class TutorSignUpController{
     public ResponseEntity<?> delete(@PathVariable("sid") long sid, @PathVariable("tid") long tid,
                                     OAuth2AuthenticationToken token) {
         String role = authControllerAdvice.getRole(token);
-        if (!role.equals("Tutor")) {
+        if (!authControllerAdvice.getIsTutor(token)) {
         return new ResponseEntity<>(HttpStatus.FORBIDDEN);
         }
 
