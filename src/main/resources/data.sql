a6302dd4aeecc3073913b03cf65f4c9bd458a0a6
Tanay Komarlu <54658599+Ulramok@users.noreply.github.com>
2020-03-08 15:50:51 -0700

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,7 +9,6 @@ INSERT INTO course_offering (course_id,quarter,instructor_name, instructor_email
 INSERT INTO course_offering (course_id,quarter,instructor_name, instructor_email) VALUES ('CMPSC 160','F19','Ding', 'ding@ucsb.edu');
 INSERT INTO course_offering (course_id,quarter,instructor_name, instructor_email) VALUES ('CMPSC 130A','W20','Koc', 'koc@ucsb.edu');
 INSERT INTO course_offering (course_id,quarter,instructor_name, instructor_email) VALUES ('CMPSC 130B','W20','Lokshtanov', 'lokshtanov@ucsb.edu');
-
 INSERT INTO tutor (first_name,last_name, email, is_active,number_of_courses_assigned) VALUES ('Scott','Chow','scottpchow@example.org', 1,0);
 INSERT INTO tutor (first_name,last_name, email, is_active,number_of_courses_assigned) VALUES ('Zach','Sisco','zachsisco@example.org', 1,0);
 INSERT INTO tutor (first_name,last_name, email, is_active,number_of_courses_assigned) VALUES ('Yinon','Rousso','yinonRousso@example.org', 1,0);
